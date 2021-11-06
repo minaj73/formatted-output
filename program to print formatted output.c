bb407c77cdf99ae67a48baf7dde411dcf380824a
minaj73 <93118731+minaj73@users.noreply.github.com>
2021-11-06 23:29:45 +0530

    Add files via upload

---
+++ program to print formatted output.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+    printf(" student information database \n");
+
+printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",120,"Amitabh",6,95,65,166,55.3333);
+printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",6,"Raj",45,8,65,100,60.000);
+printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",96,"Virat",65,85,50,200,66.666);
+
+
+    getch();
+    return 0;
+}
